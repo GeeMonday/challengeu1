6fba4aaea0d36dcdd872a7fd27c5ed022253aa62
GeeMonday <gmonday@academic.rrc.ca>
2024-05-18 16:05:56 -0500

    added new file

--- carl_sagan.rb
+++ carl_sagan.rb
@@ -21,3 +21,21 @@ sagan = [
 example = [ 'test', 'a', 'is']
 time    = { :that => 'This', :period => '.'}
 puts "#{time[:that]} #{example[2]} #{example[1]} #{example[0]}#{time[:period]}"
+
+# main Assignment
+puts "#{carl[:words][2]} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}"
+
+# task 2
+ghosts = [
+  { name: 'Inky', age: 4, loves: 'reindeers', net_worth: 25 },
+  { name: 'Pinky', age: 5, loves: 'garden tools', net_worth: 14 },
+  { name: 'Blinky', age: 7, loves: 'ninjas', net_worth: 18.03 },
+  { name: 'Clyde', age: 6, loves: 'yarn', net_worth: 0 }
+]
+
+ghosts.each do |ghost|
+  ghost_info  = "#{ghost[:name]} is #{ghost[:age]} years old, "
+  ghost_info += "loves #{ghost[:loves]} and "
+  ghost_info += "has #{ghost[:net_worth]} dollars in the bank."
+  puts ghost_info
+end
